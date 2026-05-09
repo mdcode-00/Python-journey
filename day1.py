7836a61e67ca71ab49f0b18df90de1de0ca654cd
mohd <mh408914@gmail.com>
2026-05-09 11:51:17 +0530

    Day-1 -Blloeans, Number, Floor division

---
+++ day1.py
@@ -0,0 +1,39 @@
+name = "mohd"
+age = 22
+is_developer = True
+
+print(f"my name is {name}")
+print(f"i am a developer: {is_developer}")
+
+# Math is what you would expect
+# print(1 + 1)  # => 2
+# print(8 - 1)   # => 7
+# print(10 * 2)  # => 20
+# print(35 / 5)  # => 7.0
+
+# Floor division rounds towards negative infinity
+# print(5 // 3)       # => 1
+# print(-5 // 3)      # => -2
+# print(5.0 // 3.0)   # => 1.0  # works on floats too
+# print(-5.0 // 3.0)  # => -2.0
+
+
+# The result of division is always a float
+# print(10.0 / 3)  # => 3.3333333333333335
+
+#  modulo operator gives the remainder of the division
+# print(7%3) # => 1
+# i % j hane same sigh an j ,, unlike floats too
+# print(-7%3) # => 2
+
+# exponentiation operator
+# print(2**3) # => 8
+# print(3**2) # => 9
+
+ # enforce precedence with parentheses
+# print(1 + 3 *2 ) # => 7
+# print((1+3)*2) # => 8
+
+# Bollen values are primitives (note: the capitalization matters)
+print(True)  # => True
+print(False) # => False
